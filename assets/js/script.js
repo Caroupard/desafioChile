e3c36edceeed58aea7902b71d00e600e2a1a339a
Caro Pardo <tallerdigital.carolina@gmail.com>
2022-06-30 20:19:55 -0400

    inicializamos repositorio

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+function myFunction1() {
+  var x = document.getElementById("c1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunction2() {
+  var x = document.getElementById("c2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var x = document.getElementById("c3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
